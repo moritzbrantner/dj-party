235dbdc8029feded88784d32a79b662897dbfcc2
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-12 15:14:54 +0200

    feat: install archive and playlist library imports

--- web/app.js
+++ web/app.js
@@ -1,4 +1,14 @@
+import { installLibraryImports } from "./library-imports.js";
 import { installTrackLibrary } from "./library.js";
 
-installTrackLibrary();
+const library = installTrackLibrary();
+if (library) {
+  const stylesheet = document.createElement("link");
+  stylesheet.rel = "stylesheet";
+  stylesheet.href = new URL("./library-imports.css", import.meta.url).href;
+  stylesheet.dataset.libraryImportsStyles = "true";
+  document.head.append(stylesheet);
+  installLibraryImports(library);
+}
+
 await import("./mixer-app.js");
