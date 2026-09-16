41ff21a553a3eeb23f58549a1088065b0f814f6a
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-16 22:59:18 +0200

    types: declare generated dj-party Wasm boundary

---
+++ web/pkg/dj_party.d.ts
@@ -0,0 +1,18 @@
+export default function init(...args: any[]): Promise<any>;
+
+export const CuePoint: any;
+export const Mixer: any;
+export const MonitorMixer: any;
+export const HotCueBank: any;
+
+export function analyze_rhythm(...args: any[]): any;
+export function waveform_extrema(...args: any[]): any;
+export function plan_deck_tone(...args: any[]): any;
+export function locate_beat_position(...args: any[]): any;
+export function plan_beat_jump(...args: any[]): any;
+export function plan_phase_sync(...args: any[]): any;
+export function effective_bpm(...args: any[]): any;
+export function plan_beat_loop(...args: any[]): any;
+export function plan_sync(...args: any[]): any;
+export function playback_rate_for_tempo(...args: any[]): any;
+export function tempo_percent_for_rate(...args: any[]): any;
